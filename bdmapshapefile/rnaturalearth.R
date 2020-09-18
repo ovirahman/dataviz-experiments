10a477bdd706a6bd49d68fff93f447b69cde236c
Ovi Rahman <m.ovirahman@outlook.com>
2020-09-18 17:02:18 +0600

    ..

---
+++ bdmapshapefile/rnaturalearth.R
@@ -0,0 +1,20 @@
+sp::plot(ne_states(country = "bangladesh"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################packages
+remotes::install_github("https://github.com/ropensci/rnaturalearthhires")
+install.packages("rnaturalearth")
+install.packages("rnaturalearthdata")
--- bdmapshapefile/script.R
+++ bdmapshapefile/script.R
@@ -23,10 +23,49 @@ district <- data.frame(NAME_2 = district$District, Population_Density = district
 
 map_and_data <- inner_join(map_dist, district)
 
-tm_shape(map_and_data) + tm_polygons("Population_Density",id = "NAME_2",palette = "Reds")
-tmap_mode("view")
 tmap_mode("plot")
+tm_shape(map_and_data) + tm_polygons("Population_Density",id = "NAME_2",palette = "Reds")
+
 
 tmap_mode("view")
+tm_shape(map_and_data) + tm_polygons("Population_Density",id = "NAME_2",palette = "Reds")
+
+
+
 tm_shape(map_and_data) +
     tm_bubbles(size = "Population_Density", col = "red", id = "NAME_2")
+
+
+
+
+########bbs shapefile
+
+map_dist <- st_read("bgd_admbnda_adm2_bbs_20180410/bgd_admbnda_adm2_bbs_20180410.shp")
+district <- read.csv("Districts.csv")
+district <- data.frame(District = district$District, Density = district$Pop.Density.in.2011)
+district$District <- gsub(" District","",district$District)
+district <- data.frame(ADM2_EN = district$District, Population_Density = district$Density)
+
+map_and_data <- inner_join(map_dist, district)
+
+tmap_mode("plot")
+tm_shape(map_and_data) + tm_polygons("Population_Density",id = "ADM2_EN",palette = "Reds")
+
+
+tmap_mode("view")
+tm_shape(map_and_data) + tm_polygons("Population_Density",id = "ADM2_EN",palette = "Reds")
+
+
+
+#############division
+map_dist <- st_read("")
+
+
+
+
+################sunamganj dist
+map <- st_read("bgd_admbnda_adm4_bbs_20180410/bgd_admbnda_adm4_bbs_20180410.shp")
+map2 <- filter(map, ADM2_EN == "Sunamganj")
+tm_shape(map2) + tm_polygons("ADM4_EN",id = "ADM4_EN", legend.show = F)
+
+
