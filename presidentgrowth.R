156c2223e7beeed4e155e7e85712d038ee54b679
Ovi Rahman <m.ovirahman@outlook.com>
2020-10-13 10:55:35 +0600

    ..

--- presidentgrowth.R
+++ presidentgrowth.R
@@ -7,18 +7,22 @@ bp <- barplot(data$growth~data$president, yaxt = "n",xaxt = "n",ylab = "",xlab =
               ylim = c(-10,28))
 #text(bp,0, data$growth, pos = 3, cex = 0.7)
 #text(bp,data$growth, label = paste0(data$growth, "M"), pos = 3, cex = 0.9)
-text(bp[data$growth>0],-0.3, label = paste0(data$growth[data$growth>0], "M"), pos = 3, cex = 0.8, col = "white")
-text(bp[data$growth<0],-1.4, label = paste0(data$growth[data$growth<0], "M"), pos = 3, cex = 0.8, col = "white")
-text(bp[data$growth>0],-1.8, label = data$president[data$growth>0], pos = 3, cex = 1)
-text(bp[data$growth<0],0, label = data$president[data$growth<0], pos = 3, cex = 1)
+text(bp[data$growth>0],-0.3, label = paste0(data$growth[data$growth>0], "M"), 
+     pos = 3, cex = 1.2, col = "white")
+text(bp[data$growth<0],-1.4, label = paste0(data$growth[data$growth<0], "M"), 
+     pos = 3, cex = 1.2, col = "white")
+text(bp[data$growth>0],-1.8, label = data$president[data$growth>0], 
+     pos = 3, cex = 1.3)
+text(bp[data$growth<0],0, label = data$president[data$growth<0], 
+     pos = 3, cex = 1.3)
 
 #abline(h = 20, lty = 2, col = rgb(0,0,0,0.1))
-text(1,20, label = "20 MILLION", cex = 0.8)
-text(1,10, label = "10 MILLION" , cex = 0.8)
-text(1,-5, label = "-5 MILLION" , cex = 0.8)
-segments(1.5,20,16,20, lty = 2, col = rgb(0,0,1,0.1))
-segments(1.5,10,16,10, lty = 2, col = rgb(0,0,1,0.1))
-segments(1.5,-5,16,-5, lty = 2, col = rgb(0,0,1,0.1))
+text(1,20, label = "20 MILLION", cex = 1.3)
+text(1,10, label = "10 MILLION" , cex = 1.3)
+text(1,-5, label = "-5 MILLION" , cex = 1.3)
+segments(1.5,20,16,20, lty = 2, col = rgb(0,0,0,0.3))
+segments(1.5,10,16,10, lty = 2, col = rgb(0,0,0,0.3))
+segments(1.5,-5,16,-5, lty = 2, col = rgb(0,0,0,0.3))
 
-text(8,26, label = "Job growth by president", cex = 2.5)
-text(3,-8,label = "Source: Federal Reserve Bank of St. Louis, \"All Employees, Total Nonfarm\" via facebook.com/barackobama/", cex = 0.6)
+text(8,26, label = "Job growth by US president", cex = 2.5)
+text(3,-8,label = "Source: Federal Reserve Bank of St. Louis, \"All Employees, Total Nonfarm\"(2020) via facebook.com/barackobama/", cex = 0.8)
