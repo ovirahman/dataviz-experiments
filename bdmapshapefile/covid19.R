73d4b697d541c3f328d4cd2268a1fdb32a0dcec8
Ovi <m.ovirahman@outlook.com>
2020-10-17 11:21:14 +0600

    covid

--- bdmapshapefile/covid19.R
+++ bdmapshapefile/covid19.R
@@ -3,25 +3,22 @@ library(tmap)
 library(tmaptools)
 library(leaflet)
 library(dplyr)
+library(readxl)
 
-
+#district level (administrative level 2) map shapefile data
 map_dist <- st_read("gadm36_BGD_shp/gadm36_BGD_2.shp")
-district <- read.csv("Districts.csv")
-district <- data.frame(District = district$District, Density = district$Pop.Density.in.2011)
-district$District <- gsub(" District","",district$District)
-district <- data.frame(NAME_2 = district$District, Population_Density = district$Density)
 
-
-
-
-map_and_data <- inner_join(map_dist, district)
-
+#covid19 data for 10 october
 data<- readxl::read_excel("COVID-19.xlsx", skip = 1)
 
+# map and data joined
 covi19map <- inner_join(map_dist, data, by = c("NAME_2" = "Distirct"))
 
 
+# map mode to static mode
 tmap_mode("plot")
+
+#defining and styling the map
 map <- tm_shape(covi19map) + 
   tm_polygons("Confirmed_Cases",id = "NAME_2",palette = "Reds",style = "log10_pretty", title = "Confirmed Cases") +
   tm_style("cobalt")+
@@ -31,11 +28,14 @@ map <- tm_shape(covi19map) +
     legend.position = c("right", "top")
     )
 
+# plotting the map
 map
 
-tmap_leaflet(map) %>% addControl("Bangladesh<br>Covid-19: District Wise Situation (16.10.20)<br>Source: IEDCR", position = "bottomleft")
+
+#converting it to an interactive leaflet map
+tmap_leaflet(map) %>% 
+  addControl("Bangladesh<br>Covid-19: District Wise Situation (16.10.20)<br>Source: IEDCR", position = "bottomleft")
 
 
 
 
-#tmap_mode("view")
